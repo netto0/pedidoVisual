231219c65f743ed0c96a265943353c52ca9e593a
Netto <orlandiomnetto@gmail.com>
2021-03-23 14:13:55 -0300

    texto atualizável na janela inicial

--- teste2.py
+++ teste2.py
@@ -1,2 +1,18 @@
-from biblio import janelas
-janelas.janelaCadastro()
+import PySimpleGUI as sg
+
+categoria = ['Celular', 'Bateria', 'Carregador']
+
+layout = [
+          [sg.Frame('pet',layout=categoria)]
+    ]
+
+window = sg.Window('CADASTRO DE PRODUTOS', layout, size=(700, 300))
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Cancelar'):
+        break
+    if event == 'Cadastrar':
+        window['-NOME-'].update(window['-CATEG-'])
+
+window.close()
