4cd2185eba96907e34457e0c7ed7638916c5438d
Netto <orlandiomnetto@gmail.com>
2021-05-07 18:41:45 -0300

    update

--- PedidoVisual6.0.py
+++ PedidoVisual6.0.py
@@ -12,31 +12,11 @@ import keyboard
 # Definir Data Padrão
 data_atual = datetime.today()
 data_texto = data_atual.strftime('%d/%m/%y')
-
-razoes = ["0 | Adriano Braga dos Santos","1 | Antônio Carlos Amaral","2 | Gilvan Delfino de Oliveira",
-          "3 | Ilma Delfino de Oliveira",          "4 | João Ferreira dos Santos","5 | José dos Reis Silva",
-          "6 | Lindraci Mendes Damascena","7 | Renato Moura Trindade",         "8 | Rodrigues & Pinheiro Ltda",
-          "9 | Santos & Souza Refeições Ltda","10| Pérola do Mucuri Sup. Dist. Alim.",
-          "11| Terezinha Joaquina Silva & Cia","12| Hélia Lacerda Machado","13| M. dos Santos Pereira & Cia Ltda",
-          "14| Maria Cleuza Costa & Cia Ltda","15| Adão Afonso da Silva","16| Prado & Prado Com. de Alim.",
-          "17| Débora Gil Alves Silva","18| Eunilto Maia Santos","19| Lidiomar Chaves Resende",
-          "20| Lucilene Dias Barbosa",          "21| Reinaldo Ferreira da Silva","22| Roberlan Medeiros",
-          "23| Manoelton Santos de Araújo","24| Supermercado Brás Ltda",         "25| Valdívio Leles dos Santos",
-          "26| Jaílson de Jesus","27| Jaílton Ferreira dos Santos","28| José Clemente de Jesus",
-          "29| Roberto Gomes da Silva","30| Alas da Silva Santos","31| Claudio Souza Cortes",
-          "32| Edenaldo Santana Souza",         "33| Ismar Costa Mendes Lima","34| Jorvane Antônio Lima",
-          "35| José Afonso Faria","36| Josenélia Farias Lucas",         "37| Maria Emília Silva de Souza",
-          "38| Márcio Simão da Silva","39| Mercearia Mineira Ltda","40| Núbia C. B. Leite",
-          "41| Oliveira & Leite Ltda","42| Sílvio Cláudio Com. Prod. Alim.","43| Valdomiro Oliveira",
-          "44| Adão Brandão dos Santos",          "45| Adilson Ramos Pereira","46| Afrodízio Tenencio de Brito",
-          "47| Da Hora e Soares Ltda","48| Fábio de Souza Bom Jardim",          "49| João Souza dos Santos",
-          "50| Raquel Pereira Mota","51| Laís Lima Brito","52| Maria da Glória de Jesus Viana","53| Ademir Galdino",
-          "54| Davirlei de Jesus Costa","55| Detrez Azevedo Com. Prod. Alim.","56| Edgard Rocha Santos"
-          ,"57| Eric Melo de Oliveira","58| Hélcio Ramos Sobral","59| Janilson Soares Araújo",
-          "60| José Roberto Dias do Amaral",          "61| Valdirene Bremer Ramalho","62| Milton Alves de Almeida",
-          "63| Zilberto Freitas Meireles","64| Fabiano Folgado",          "65| Rosilene L. Espíndola",
-          "66| Aline Almeida Lacerda","67| Valdemir Pereira Santos",
-]
+razoes = []
+def give_razao():
+    for c in range(0,68):
+        razoes.append(clientes.itensArquivo(c)[0])
+give_razao()
 
 #Preços Padrão
 precobb = 175
@@ -59,7 +39,7 @@ while True:
             [sg.I(key='-SEARCH-',size=(55,1),enable_events=True)],
             [sg.Listbox(razoes, size=(55, 20), key='-RAZOES-')],
             [sg.Button('OK'), sg.Button('Cadastrar')],
-            [sg.Text(f'Data:'), sg.Combo(datas,size=(9,1),key='-DATA-',default_value=datas[0])],
+            [sg.Text(f'Data:'), sg.I(size=(9,1),key='-DATA-',default_text=data_texto,),sg.B('Hoje')],
             [sg.Text(f'Razão:',key='razaoPedido',size=(40,1))],
             [sg.Text(f'Forma:'),sg.Combo(formaPagamento, key='-FORMA-',size=(5,1)),sg.Text('Prazo:'),
              sg.Combo(prazoPagamento,key=('-PRAZO-'))],
@@ -136,24 +116,40 @@ while True:
 
 
     nova_lista = []
-    def search(nome, lista):
+
+
+    def search(nome, lista, indice=False):
         nova_lista = []
         to_ascii(lista)
-        ref =unidecode(nome)
+        ref = unidecode(nome)
         for l in lista:
             if re.findall(rf'{ref}', l, flags=re.I) != []:
                 numero = lista.index(l)
                 nova_lista.append(razoes[numero])
             else:
                 None
-        return nova_lista
+        if indice:
+            return lista.index(nova_lista[0])
+        else:
+            return nova_lista
+
 
     def kb_event_update():
-        if janela == janela1 and len(evento) == 1 or keyboard.is_pressed('\b'):
-            tamanho = len(valores['-SEARCH-'])
-            dta = search(valores['-SEARCH-'], razoes)
-            janela1.Element('-RAZOES-').update(values=dta)
+        try:
+            if janela == janela1 and len(evento) == 1 or keyboard.is_pressed('\b'):
+                dta = search(valores['-SEARCH-'], razoes)
+                janela1.Element('-RAZOES-').update(values=dta)
+        except:
+            None
 
+    def data_format():
+        try:
+            if janela == janela1 and len(evento) == 1 or keyboard.is_pressed('\b'):
+                tamanho = len(valores['-DATA-'])
+                if tamanho == 2 or tamanho == 5:
+                    janela1.Element('-DATA-').update(value=f'{valores["-DATA-"]}/')
+        except:
+            None
     # Definindo Janelas (Janela1 = Janela inicial)
     janela1,janela2,janela3,janela4 = tela_pedido(), None, None,None
 
@@ -163,20 +159,22 @@ while True:
         janela, evento, valores = sg.read_all_windows()
     #Eventos Janela1
         kb_event_update()
+        data_format()
+        data_valida = False
         #Fechar Janela
         if janela == janela1 and evento == 'Fechar':
             exit()
         if janela == janela1 and evento == sg.WIN_CLOSED:
             exit()
-
         if janela == janela1 and evento == 'Cadastrar':
             janela1.hide()
             janela2 = janelas.janelaCadastro()
-
+        if janela == janela1 and evento == 'Hoje':
+            janela1.Element('-DATA-').update(value=data_texto)
         if janela == janela1 and evento == 'OK':
             try:
                 if valores['-RAZOES-']:  # if something is highlighted in the list
-                    codigoPedidoPrevia = int(valores['-RAZOES-'][0][:2])
+                    codigoPedidoPrevia = search(valores['-RAZOES-'][0],razoes,indice=True)
                 clientePrevia = clientes.itensArquivo(codigoPedidoPrevia)
                 razao = clientePrevia[0]
                 formaPrevia = clientePrevia[2]
@@ -295,7 +293,6 @@ while True:
         if janela == janela1 and evento == 'Enviar':
 
             try:
-                codigoPedido = int(valores['codigo_cliente'])
                 precobb1 = valores['prc_bb_1kg']
                 precobb2 = valores['prc_bb_2kg']
                 precobb5 = valores['prc_bb_5kg']
@@ -315,9 +312,8 @@ while True:
                 qtdSCBBPedido = int(valores['qtd_sc_bb'])
                 qtdSCGVPedido = int(valores['qtd_sc_gv'])
                 obsPedido = valores['obs']
-
                 date = valores['-DATA-']
-                codigo = codigoPedido
+                codigo = codigoPedidoPrevia
                 cliente = clientes.itensArquivo(codigo)
                 nome_cliente = cliente[0]
                 cidade1 = cliente[1]
@@ -338,6 +334,8 @@ while True:
                     notaPedido = ''
                     janela1.hide()
                     break
+                if len(valores["-DATA-"]) != 8:
+                    print('Digite uma data válida!')
             except Exception as e:
                 print('Digite um código válido')
                 print(TypeError,e)
